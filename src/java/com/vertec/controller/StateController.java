583232e302d0f027cb6160e53f3ae5bfbbbae2a3
Ruchira <Ruchira@Ruchira>
2017-03-31 08:58:49 +0530

    Merge origin/master
    Conflicts:
    	src/java/com/vertec/controller/ProductController.java

---
+++ src/java/com/vertec/controller/StateController.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vertec.controller;
+
+import com.vertec.daoimpl.StateDAOImpl;
+import com.vertec.hibe.model.State;
+import com.vertec.hibe.model.SysUser;
+import com.vertec.util.VertecConstants;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Ruchira
+ */
+@WebServlet(name = "StateController", urlPatterns = {"/StateController"})
+public class StateController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    private final StateDAOImpl statedao = new StateDAOImpl();
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            String action = request.getParameter("action");
+            HttpSession httpSession = request.getSession();
+            SysUser user1 = (SysUser) httpSession.getAttribute("user");
+            RequestDispatcher requestDispatcher;
+            switch(action){
+                
+                case "viewStatePage": {
+                    
+                    requestDispatcher = request.getRequestDispatcher("app/account/state/registerState.jsp");
+                    requestDispatcher.forward(request, response);
+                    break;
+                }
+                case "saveState": {
+                    String state = request.getParameter("name").trim();
+                    State s = new State();
+                    s.setName(state);
+                    
+                    String result = statedao.saveState(s);
+                    if (result.equals(VertecConstants.SUCCESS)) {
+                        request.getSession().removeAttribute("Success_Message");
+
+                        request.getSession().setAttribute("Success_Message", "Successfully Added");
+                        response.sendRedirect("State?action=viewStatePage");
+                    } else {
+                        request.getSession().removeAttribute("Error_Message");
+
+                        request.getSession().setAttribute("Error_Message", "Not Added,Please Tri again");
+                        response.sendRedirect("State?action=viewStatePage");
+                    }
+                }
+            }
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
