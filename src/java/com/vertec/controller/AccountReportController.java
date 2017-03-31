30ee5034552ee24fd95be24959b1a703c27c0454
rohanmadusankavertec <vertecdeveloper.rohan@gmail.com>
2017-03-31 08:53:01 +0530

    Merge origin/master
    Conflicts:
    	src/java/com/vertec/controller/InvoiceController.java
    	web/app/invoice/CreateInvoice.jsp

--- src/java/com/vertec/controller/AccountReportController.java
+++ src/java/com/vertec/controller/AccountReportController.java
@@ -25,8 +25,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
 /**
  *
